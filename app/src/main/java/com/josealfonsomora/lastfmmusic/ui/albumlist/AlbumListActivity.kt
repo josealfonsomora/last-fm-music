60ff459a7e4168750dac8ab55a9337cece133349
Jose Alfonso Mora <josealfonsomora@gmail.com>
2020-06-26 16:32:30 +0200

    Add throtthling before requesting album data
    Because it is attached to an edittext we need to throtthling it
    avoid multiple consecutive calls
    
    Removed Nested scrool that was freezing the ui and was not
    necessary

--- app/src/main/java/com/josealfonsomora/lastfmmusic/ui/albumlist/AlbumListActivity.kt
+++ app/src/main/java/com/josealfonsomora/lastfmmusic/ui/albumlist/AlbumListActivity.kt
@@ -6,14 +6,23 @@ import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.josealfonsomora.lastfmmusic.R
-import com.josealfonsomora.lastfmmusic.ui.albuminfo.AlbumInfoActivity
+import com.josealfonsomora.lastfmmusic.core.extensions.disposeWith
 import com.josealfonsomora.lastfmmusic.databinding.ActivityAlbumListBinding
+import com.josealfonsomora.lastfmmusic.ui.albuminfo.AlbumInfoActivity
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.concurrent.TimeUnit
+
+const val VIEW_CLICK_THROTTLE_DELAY_MS = 300L
 
 class AlbumListActivity : ComponentActivity() {
     private lateinit var binding: ActivityAlbumListBinding
+    private val disposables = CompositeDisposable()
 
     private val viewModel: AlbumListViewModel by viewModel()
+    private val emitter = PublishSubject.create<CharSequence>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +39,14 @@ class AlbumListActivity : ComponentActivity() {
         binding.toolbar.title = getString(R.string.app_name)
 
         binding.searchBar.doOnTextChanged { text, _, _, _ ->
-            text?.let { viewModel.searchAlbum(it) }
+            text?.let { emitter.onNext(text) }
         }
+
+        emitter
+            .throttleFirst(VIEW_CLICK_THROTTLE_DELAY_MS, TimeUnit.MILLISECONDS, Schedulers.io())
+            .subscribe {
+                viewModel.searchAlbum(it)
+            }.disposeWith(disposables)
+
     }
 }
